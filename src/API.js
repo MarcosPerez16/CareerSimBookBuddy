59c8dfe4a7695e10a2e177eaadf01efa261bddde
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-02-15 11:40:13 -0800

    created GET request to fetch books api data, rendered data on homepage

--- src/API.js
+++ src/API.js
@@ -1,3 +1,20 @@
 //remember in order to use as an export start each function with:
 // export async function functionName(){}
 //then can be imported into a component
+
+const APIURL = "https://fsa-book-buddy-b6e748d1380d.herokuapp.com";
+
+//fetch books
+
+export const fetchBooks = async () => {
+  const response = await fetch(`${APIURL}/api/books`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  console.log(data);
+
+  return data.books;
+};
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,11 +4,32 @@ Fetch the book data from the provided API.
 Users should be able to click on an individual book
  to navigate to the SingleBook component and view its details. */
 // imports here
+
+import { useState, useEffect } from "react";
+import { fetchBooks } from "../API";
+
 const Books = () => {
   // logic here
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooksList = async () => {
+      const booksData = await fetchBooks();
+      setBooks(booksData);
+    };
+    getBooksList();
+  }, []);
+
   return (
     <div>
-      <h2>This page will show all the books in the library's catalog</h2>
+      <h2 style={{ textAlign: "center" }}>Library Catalog</h2>
+      {books.map((book) => (
+        <div style={{ textAlign: "center" }} key={book.id}>
+          <h3>{book.title}</h3>
+          <p>Author: {book.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
